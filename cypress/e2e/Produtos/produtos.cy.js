52a63ca09e2c4bf7263ef2a77f0eba729565b990
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-04-17 11:20:59 -0300

    Adicionando caso de testes
    Adicionado casos de testes de falha ao tentar adicionar item sem cor e tamanho ao carrinho e refatorando entrada de parâmetros

--- cypress/e2e/Produtos/produtos.cy.js
+++ cypress/e2e/Produtos/produtos.cy.js
@@ -1,4 +1,6 @@
 import ProdutosPage from "../../pages/ProdutosPage/ProdutosPage";
+import { NotificacaoElements } from "../../elements/NotificacaoElements";
+
 beforeEach(() => {
   ProdutosPage.visit();
 });
@@ -8,11 +10,25 @@ describe("Testes da funcionalidade de produtos", () => {
     ProdutosPage.visualizarProdutos();
   });
 
-  it("Deve visualizar os detalhes de um produto cadastrado com sucesso", () => {
-    ProdutosPage.acessarProduto();
+  it("Deve visualizar os detalhes de um produto com sucesso", () => {
+    ProdutosPage.acessarProduto("Breathe-Easy Tank");
   });
 
-  it.only("Deve adicionar um produto ao carrinho", () => {
-    ProdutosPage.adicionarProdutoAoCarrinho();
+  it("Deve adicionar um produto ao carrinho", () => {
+    ProdutosPage.adicionarProdutoAoCarrinho("Breathe-Easy Tank", "S", "White");
+  });
+
+  it("Deve falhar ao tentar adicionar um produto no carrinho sem selecionar o tamanho", () => {
+    ProdutosPage.adicionarProdutoAoCarrinho("Breathe-Easy Tank", "", "White");
+    cy.get(NotificacaoElements.errorMessage)
+      .should("be.visible")
+      .and("contain.text", "Incorrect or missing options!");
+  });
+
+  it("Deve falhar ao tentar adicionar um produto no carrinho sem selecionar a cor", () => {
+    ProdutosPage.adicionarProdutoAoCarrinho("Breathe-Easy Tank", "S", "");
+    cy.get(NotificacaoElements.errorMessage)
+      .should("be.visible")
+      .and("contain.text", "Incorrect or missing options!");
   });
 });
