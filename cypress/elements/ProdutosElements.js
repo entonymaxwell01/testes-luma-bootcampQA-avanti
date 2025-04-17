de4c36f148b1f626e1850cb8816d7231ea3671f6
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-04-16 21:52:59 -0300

    Criando arquivo de elementos de produtos

---
+++ cypress/elements/ProdutosElements.js
@@ -0,0 +1,10 @@
+export const ProdutosElements = {
+  produtosList: ".ProductListPage",
+  produtoCard: ".ProductCard-LinkInnerWrapper",
+  produtoName: ".ProductCard-Name",
+  produtoTitle: ".ProductPage-Title",
+  produtoPrice: ".ProductPrice-PriceValue",
+  produtoAddToCart: ".ProductActions-AddToCart",
+  produtoSelectColor: "#color",
+  produtoSelectSize: "#size",
+};
